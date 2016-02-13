8a771afc9422084138214d7eecda667662f9e8a4
Grover Irusta Miranda <grover_ariel@hotmail.com>
2016-02-13 00:55:54 -0400

    Create Bill.java

---
+++ .urionlinejudge/src/BEGINNER/Bill.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Bill {
+
+	public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	int n  =in.nextInt();
+	while (n-->0)
+	{
+		int m = in.nextInt();
+		if (m%2==0) 
+		{
+			System.out.println(0);
+		}
+		else
+			System.out.println(1);
+		
+	}
+
+	}
+
+}
