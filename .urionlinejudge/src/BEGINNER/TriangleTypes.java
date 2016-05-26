c2915cec0aa54174438b188765621cfd1c831b74
Grover Irusta Miranda <grover_ariel@hotmail.com>
2016-05-26 00:23:22 -0400

    Add files via upload

--- .urionlinejudge/src/BEGINNER/TriangleTypes.java
+++ .urionlinejudge/src/BEGINNER/TriangleTypes.java
@@ -8,28 +8,50 @@ public class TriangleTypes {
 	public static void main(String[] args) 
 	{
 		Scanner in  = new Scanner(System.in);
-		while (in.hasNextDouble()) {
-			
+
 		
 		double V[]= new double[3];
 		V[0]= in.nextDouble();
 		V[1]= in.nextDouble();
 		V[2]= in.nextDouble();
-	      //  Arrays.sort(V);
+		double aux = 0;
+		if (V[0] <V[1])
+	    {
+	        aux = V[0];
 
-	        if (V[0] > V[1] + V[2])
-	            System.out.println("NAO FORMA TRIANGULO");  
-	        if (V[0]*V[0] == V[1]*V[1] + V[2]*V[2])
-	        	System.out.println( "TRIANGULO RETANGULO"); 
-	        if (V[0]*V[0] > V[1]*V[1] + V[2]*V[2])
-	        	System.out.println("TRIANGULO OBTUSANGULO"); 
-	        if (V[0]*V[0] < V[1]*V[1] + V[2]*V[2])
-	        	System.out.println( "TRIANGULO ACUTANGULO"); 
-	        if (V[0] == V[1] && V[1] == V[2])
-	        	System.out.println("TRIANGULO EQUILATERO"); 
-	        if (V[0] == V[1] || V[1] == V[2]|| V[0] == V[2])
-	        	System.out.println( "TRIANGULO ISOSCELES"); 
+	        V[0] = V[1];
+
+	        V[1] = aux;
 	    }
+	    if (V[1] < V[2])
+	    {
+	        aux = V[1];
+
+	        V[1] = V[2];
+
+	        V[2] = aux;
+	    }
+	    if (V[0]< V[1])
+	    {
+	        aux = V[0];
+
+	        V[0] = V[1];
+
+	        V[1] = aux;
+	    }
+	        if (V[0] >= V[1] + V[2])
+	            System.out.println("NAO FORMA TRIANGULO");  
+	        else if (V[0]*V[0] == (V[1]*V[1]) + (V[2]*V[2]))
+	        	System.out.println( "TRIANGULO RETANGULO"); 
+	        else if (V[0]*V[0] > (V[1]*V[1]) + (V[2]*V[2]))
+	        	System.out.println("TRIANGULO OBTUSANGULO"); 
+	        else if (V[0]*V[0] < (V[1]*V[1]) + (V[2]*V[2]))
+	        	System.out.println( "TRIANGULO ACUTANGULO"); 
+	          if (V[0] == V[1] && V[1] == V[2])
+	        	System.out.println("TRIANGULO EQUILATERO"); 
+	        else  if (V[0] == V[1] || V[1] == V[2])
+	        	System.out.println( "TRIANGULO ISOSCELES"); 
+
 	}
 
 	}
