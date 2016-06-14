3f0bf77c09533e2944512d6a0da0b125f6efd06f
Grover Irusta Miranda <grover_ariel@hotmail.com>
2016-06-14 00:05:41 -0400

    Add files via upload

---
+++ .omegaup/src/euclides.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+
+public class euclides {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println(in.nextBigInteger().gcd(in.nextBigInteger()));
+
+	}
+
+}
