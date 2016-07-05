e835021aa1c810ff90f21760dd80b6ba87a76135
Grover Irusta Miranda <grover_ariel@hotmail.com>
2016-07-05 14:45:52 -0400

    Add files via upload

---
+++ .Uva/src/ILoveBigNumbers10220.java
@@ -0,0 +1,24 @@
+
+
+import java.util.*;
+import java.math.*;
+
+public class ILoveBigNumbers10220 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		while (in.hasNext()) {
+			int n = in.nextInt();
+			BigInteger fact = BigInteger.ONE;
+			
+			for (int i = 2; i <= n; ++i)
+				fact = fact.multiply(BigInteger.valueOf(i));
+			String cad = fact.toString();
+			//System.out.println(fact);
+			int sum = 0;
+			for (int i = 0; i < cad.length(); ++i)
+				sum += Integer.parseInt(cad.charAt(i)+"");
+			System.out.println(sum);
+		}
+	}
+}
