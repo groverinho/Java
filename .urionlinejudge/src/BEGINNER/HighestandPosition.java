93fa1dbf0812402b31ad1905e95c95db2b28cf72
Grover Irusta Miranda <grover_ariel@hotmail.com>
2016-07-31 21:27:05 -0400

    Update HighestandPosition.java

--- .urionlinejudge/src/BEGINNER/HighestandPosition.java
+++ .urionlinejudge/src/BEGINNER/HighestandPosition.java
@@ -1,34 +1,32 @@
 package BEGINNER;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.Scanner;
 
+
+import java.util.Scanner;
 
 public class HighestandPosition {
 
-	public static void main(String[] args)
-	{
-		Scanner in = new Scanner(System.in);
-		ArrayList<Integer>lista = new ArrayList<Integer>();
-		int n [] = new int [11];
-		
-		for (int i = 1; i < n.length; i++) 
-		{
-			n[i] = in.nextInt();
-			lista.add(n[i]);
-		}
-		Arrays.sort(n);
-		int aux = n[10];
-		int cont=0;
-		//int aa = Arrays.binarySearch(n, aux); raro!
-		
-		for (int i = 0; i < lista.size(); i++, cont++)
-		{
-			if (lista.get(i)==aux)
-				break;
-		}
-		System.out.println(aux+"\n"+(cont+1));
-	}
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int index = 0;
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 1; i <= 100; i++){
+
+            int x = in.nextInt();
+
+            if(x > max){
+                max = x;
+                index = i;
+            }
+
+        }
+
+        System.out.println(max);
+        System.out.println(index);
+
+    }
 
 }
+
