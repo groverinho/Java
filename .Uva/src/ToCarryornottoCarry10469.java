42f319e07b623dbef2c48026025f51c8a6b7c792
Grover Irusta Miranda <grover_ariel@hotmail.com>
2016-08-17 01:07:11 -0400

    Update ToCarryornottoCarry10469.java

--- .Uva/src/ToCarryornottoCarry10469.java
+++ .Uva/src/ToCarryornottoCarry10469.java
@@ -6,14 +6,10 @@ public class ToCarryornottoCarry10469 {
 	public static void main(String[] args) 
 	{
 		Scanner in =new Scanner(System.in);
-		while (in.hasNextBigInteger()) 
-		{
-	  BigInteger a= in.nextBigInteger();
-		BigInteger b= in.nextBigInteger();
-		BigInteger n = null;
-		   n = a.or(b);
-		  System.out.println(n);
-		  }
+		//String cad = "";
+		 while (in.hasNextLong())
+	            System.out.println(in.nextLong() ^ in.nextLong());
+
 	
 	}
 
