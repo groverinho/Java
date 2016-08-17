28274b536e7c1b86888d2c83aed753a6acc75b7d
Grover Irusta Miranda <grover_ariel@hotmail.com>
2016-08-17 01:21:37 -0400

    Add files via upload

---
+++ .Uva/src/HashmattheBraveWarrior10055.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class HashmattheBraveWarrior10055 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		while (in.hasNext()) {
+			long n = in.nextLong();
+			long n2 = in.nextLong();
+			System.out.println(Math.abs(n-n2));
+			
+		}
+
+	}
+
+}
