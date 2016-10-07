2a83b93895397d30b64770257cbbbd8a1c643cb9
Grover Irusta Miranda <grover_ariel@hotmail.com>
2016-10-07 10:06:05 -0400

    Update Aburrido.java

--- .omegaup/src/Aburrido.java
+++ .omegaup/src/Aburrido.java
@@ -6,19 +6,21 @@ public class Aburrido {
 	public static void main(String[] args)
 	{
 		Scanner in = new Scanner(System.in);
-	//int n = in.nextInt();
-		int vector []= new int [100];
-		int vector2 []= new int [100];
+	int n = in.nextInt();
+		int vector []= new int [100000];
+		
 		vector[0]=3;
-
 		int aux = 3;
 		for (int i = 1; i < vector.length; i++) 
 		{
-			aux = 2 +vector[i-1];
-			vector[i] =  +aux;
+			vector[i]= vector[i-1]+aux;
+			aux+=2;
+//			aux = 2 +vector[i-1];
+//			vector[i] = +aux;
+	//	System.out.println(vector[i]);
 			
-			System.out.println(vector[i]);
 		}
+		System.out.println(vector[n-1]);
 
 	}
 
