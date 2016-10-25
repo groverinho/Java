edc7c46f5a8b51acde64837396224d364f032572
Grover Irusta Miranda <grover_ariel@hotmail.com>
2016-10-24 23:21:23 -0400

    Add files via upload

---
+++ .urionlinejudge/src/BEGINNER/BobConduit.java
@@ -0,0 +1,24 @@
+package BEGINNER;
+import java.util.Scanner;
+
+/**
+Name: Grover Irusta Miranda
+University: UPDS Tarija
+Date: 18-10-16
+ */
+
+public class BobConduit {
+
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int casos = in.nextInt();
+		while (casos-->0)
+		{
+			int n  = in.nextInt();
+			int m  = in.nextInt();
+			System.out.println(n+m);
+		}
+	}
+
+}
