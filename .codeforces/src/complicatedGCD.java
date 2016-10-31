a4685dcab34e6b8a6d8cdbbb4458d03730cc4cba
Grover Irusta Miranda <grover_ariel@hotmail.com>
2016-10-31 15:29:24 -0400

    Add files via upload

---
+++ .codeforces/src/complicatedGCD.java
@@ -0,0 +1,24 @@
+import java.math.BigInteger;
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+/**
+Name: Grover Irusta Miranda
+University: UPDS Tarija
+Date: 31-10-16
+ */
+
+public class complicatedGCD {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		String a  =in.next(), b  =in.next();
+		System.out.println(a.equals(b)?a:"1");
+
+	}
+
+}
