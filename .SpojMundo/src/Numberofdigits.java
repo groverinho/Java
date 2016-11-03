35b3db6a94ee2fe3c111f2546243539c68a0a4c4
Grover Irusta Miranda <grover_ariel@hotmail.com>
2016-11-03 16:14:44 -0400

    Add files via upload

---
+++ .SpojMundo/src/Numberofdigits.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Numberofdigits{
+public static void main(String[] args){
+Scanner c = new Scanner(System.in);
+int x = c.nextInt();
+while (x-->0)
+System.out.println(c.next().length());
+}
+}
