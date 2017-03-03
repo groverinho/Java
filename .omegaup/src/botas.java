c611744ca4c1b2127eb1ebf8483f584441a9e96f
Grover Irusta Miranda <grover_ariel@hotmail.com>
2017-03-03 08:18:44 -0400

    Update botas.java

--- .omegaup/src/botas.java
+++ .omegaup/src/botas.java
@@ -1,33 +1,22 @@
-import java.util.Arrays;
 import java.util.Scanner;
-
 
 public class botas {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner in = new Scanner( System.in);
+		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
-		int array[]= new int [n];
-		int impares=0, i=0;
-	    for(i=0;i<n;i++)
-	        array[i] = in.nextInt();
+		int []cartas = new int [1000000];
+		for (int i = 0; i < n; i++) {
+			cartas[in.nextInt()]++;
+		}
+		int c =0;
+		for (int i = 0; i < cartas.length; i++) {
+			if (cartas[i]%2!=0) {
+				c++;
+			}
+		}
+		System.out.println(c);
 
-	  Arrays.sort(array);
-
-	    for(i=0;i<n;i+=2)
-	    {
-	    	if (i+1!=n) {
-				
-			
-	        if(array[i]!=array[i+1])
-	        {
-	            impares++;
-	            i--;
-	        }
-	        }
-	    }
-	    System.out.println(impares+1);
 	}
 
 }
