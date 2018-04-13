75f512e419a46f13bdd3fec0fa7aeeaeabbf1b4f
Grover Irusta Miranda <grover_ariel@hotmail.com>
2018-04-13 17:00:05 -0400

    Add files via upload

---
+++ .omegaup/src/aliciaYlasLlavesDoradas.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+Name: Grover A. Irusta Miranda
+Company: CodeRoad - Mojix
+Date 13/04/2018
+ */
+public class aliciaYlasLlavesDoradas {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int m = Integer.parseInt(in.readLine());
+		String chapas[] = new String[m];
+		String v1 = in.readLine();
+		chapas = v1.split(" ");
+		int n = Integer.parseInt(in.readLine());
+		String llaves[]=new String[n];
+		String v2 = in.readLine();
+		llaves = v2.split(" ");
+		for (int i = 0; i < llaves.length; i++) {
+			
+			int index = Arrays.binarySearch(chapas, llaves[i]);
+			if(index < 0){
+				System.out.println(0);
+			}
+			else{
+				System.out.println(index+1);
+			}
+		}
+		
+	}
+
+}
