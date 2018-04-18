a963ace25a7d510b5d5933e1764fc25251493ba6
Grover <grover.irusta@mojix.com>
2018-04-18 15:09:04 -0400

    acumulando monedas problem

---
+++ .omegaup/src/AcumulandoMonedas.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+Name: Grover A. Irusta Miranda
+Company: CodeRoad - xTime
+Date 18/04/2018
+ */
+
+public class AcumulandoMonedas {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println(in.nextInt()*10);
+	}
+
+}
