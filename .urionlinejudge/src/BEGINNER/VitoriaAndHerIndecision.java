0ea30367d78bc075ec6c7d7737f17e0b42e983e0
Grover <grover.irusta@mojix.com>
2018-04-27 18:04:36 -0400

    VitoriaAndHerIndecision problem solved

---
+++ .urionlinejudge/src/BEGINNER/VitoriaAndHerIndecision.java
@@ -0,0 +1,20 @@
+/**
+Name: Grover A. Irusta Miranda
+Company: CodeRoad - xTime
+Date /2018
+ */
+package BEGINNER;
+import java.util.Scanner; 
+public class VitoriaAndHerIndecision {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String n  = in.next();
+		int n2 = Integer.parseInt(n);
+		for (int i = 0; i < n2; i++) {
+			String cad = in.nextLine();
+		}
+		System.out.println("Ciencia da Computacao");
+	}
+
+}
