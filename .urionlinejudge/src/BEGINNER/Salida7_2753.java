9a027c63f8232ace32d76f3c4520ca51af1a17e9
Grover Irusta Miranda <grover_ariel@hotmail.com>
2018-05-07 15:13:11 -0400

    Add files via upload

---
+++ .urionlinejudge/src/BEGINNER/Salida7_2753.java
@@ -0,0 +1,21 @@
+/**
+Name: Grover A. Irusta Miranda
+Company: CodeRoad - xTime
+Date 07/05/2018
+ */
+package BEGINNER;
+
+import java.util.Scanner;
+
+public class Salida7_2753 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		char c = 'a';
+		for (int i = 97; i <= 122; i++) {
+			System.out.println(i + " e " + c);
+			c++;
+		}
+	}
+
+}
