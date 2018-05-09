e2c7992f96a3aa9806406ea86fd01ad0d1518cb8
Grover Irusta Miranda <grover_ariel@hotmail.com>
2018-05-09 15:36:46 -0400

    Add files via upload

---
+++ .urionlinejudge/src/BEGINNER/Salida4_2750.java
@@ -0,0 +1,32 @@
+/**
+Name: Grover A. Irusta Miranda
+Company: CodeRoad - xTime
+Date 09/05/2018
+ */
+package BEGINNER;
+
+public class Salida4_2750 {
+
+	public static void main(String[] args) {
+		System.out.print("---------------------------------------\n");
+		System.out.print("|  decimal  |  octal  |  Hexadecimal  |\n");
+		System.out.print("---------------------------------------\n");
+		System.out.print("|      0    |    0    |       0       |\n");
+		System.out.print("|      1    |    1    |       1       |\n");
+		System.out.print("|      2    |    2    |       2       |\n");
+		System.out.print("|      3    |    3    |       3       |\n");
+		System.out.print("|      4    |    4    |       4       |\n");
+		System.out.print("|      5    |    5    |       5       |\n");
+		System.out.print("|      6    |    6    |       6       |\n");
+		System.out.print("|      7    |    7    |       7       |\n");
+		System.out.print("|      8    |   10    |       8       |\n");
+		System.out.print("|      9    |   11    |       9       |\n");
+		System.out.print("|     10    |   12    |       A       |\n");
+		System.out.print("|     11    |   13    |       B       |\n");
+		System.out.print("|     12    |   14    |       C       |\n");
+		System.out.print("|     13    |   15    |       D       |\n");
+		System.out.print("|     14    |   16    |       E       |\n");
+		System.out.print("|     15    |   17    |       F       |\n");
+		System.out.print("---------------------------------------\n");
+	}
+}
