3b961e82549a9754be336875c5e8fe1c9f29f132
Grover Irusta Miranda <grover_ariel@hotmail.com>
2018-05-09 13:55:06 -0400

    Add files via upload

---
+++ .urionlinejudge/src/BEGINNER/Salida3_2749.java
@@ -0,0 +1,19 @@
+/**
+Name: Grover A. Irusta Miranda
+Company: CodeRoad - xTime
+Date 09/05/2018
+ */
+package BEGINNER;
+
+public class Salida3_2749 {
+
+	public static void main(String[] args) {
+		System.out.print("---------------------------------------\n");
+		System.out.print("|x = 35                               |\n");
+		System.out.print("|                                     |\n");
+		System.out.print("|               x = 35                |\n");
+		System.out.print("|                                     |\n");
+		System.out.print("|                               x = 35|\n");
+		System.out.print("---------------------------------------\n");
+	}
+}
