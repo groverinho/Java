1a865d84b8da3b82808dcd586135d07fa02b9e67
Grover Irusta Miranda <grover_ariel@hotmail.com>
2018-05-11 15:58:21 -0400

    Add files via upload

---
+++ .urionlinejudge/src/BEGINNER/RegularSimplePolygons.java
@@ -0,0 +1,18 @@
+/**
+Name: Grover A. Irusta Miranda
+Company: CodeRoad - xTime
+Date 11/04/2018
+ */
+package BEGINNER;
+
+import java.util.Scanner;
+
+public class RegularSimplePolygons {
+
+	public static void main(String[] args) {
+		Scanner  in = new Scanner(System.in);
+		long n = in.nextLong(), l = in.nextLong();
+		System.out.println(n*l);
+	}
+
+}
