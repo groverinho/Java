b4c661c0a13465a9f5da9159131fe6237fc61529
Grover Irusta Miranda <grover_ariel@hotmail.com>
2018-05-11 16:11:46 -0400

    Add files via upload

---
+++ .urionlinejudge/src/BEGINNER/GuilhermeAndHisKites1921.java
@@ -0,0 +1,18 @@
+/**
+Name: Grover A. Irusta Miranda
+Company: CodeRoad - xTime
+Date 11/05/2018
+ */
+package BEGINNER;
+
+import java.util.Scanner;
+
+public class GuilhermeAndHisKites1921 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		long n = in.nextLong();
+		System.out.println((n*(n-3))/2);
+	}
+
+}
