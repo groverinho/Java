82ee8b1688266a2663f30a9613bf4695cf28ec11
Grover Irusta Miranda <grover_ariel@hotmail.com>
2018-11-07 22:12:58 -0400

    Create TheMotionPicture1963.java

---
+++ .urionlinejudge/src/BEGINNER/TheMotionPicture1963.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.io.*;
+
+//Date: 07/11/18
+public class TheMotionPicture1963. {
+	public static void main(String[] args) throws Exception {
+	Scanner in = new Scanner(System.in);
+	double a = in.nextDouble();
+	double b = in.nextDouble();
+	  double c;
+
+	    c = (((b-a)*100.00)/a);
+	    System.out.printf("%.2f", c);
+	    System.out.println("%");
+	}
+}
