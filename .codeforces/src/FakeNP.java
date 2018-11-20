ed108ddc343e61e4b18a4fe2404d11584a0d2943
Grover Irusta Miranda <grover_ariel@hotmail.com>
2018-11-20 11:53:54 -0400

    Add files via upload

---
+++ .codeforces/src/FakeNP.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+Name: Grover A. Irusta Miranda
+Company: CodeRoad - xTime
+Date 20/11/2018
+ */
+
+public class FakeNP {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		long l = in.nextLong(), r = in.nextLong();
+		System.out.println(l == r ? l: 2);
+	}
+
+}
