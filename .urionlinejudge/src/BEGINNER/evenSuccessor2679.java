e7346a7f22cec216c8ec2a74e60697ac6722f85b
Grover I <grover_ariel@hotmail.com>
2020-08-30 20:59:48 -0400

    Add files via upload

---
+++ .urionlinejudge/src/BEGINNER/evenSuccessor2679.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+@author Grover Irusta M.
+@date DD/MM/2020
+@Company Mojix/Xtime
+ */
+public class evenSuccessor2679 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		System.out.println(n%2==0?n+2:n+1);
+
+	}
+
+}
