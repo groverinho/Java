9059020173e344e4f87874d8a0b3880a9cbc935c
Grover I <grover_ariel@hotmail.com>
2021-09-05 21:10:57 -0400

    Add files via upload

---
+++ .urionlinejudge/src/BEGINNER/ourDaysAreNeverComingBack1865.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ourDaysAreNeverComingBack1865 {
+
+	public static void main(String[] args) {
+		Scanner  in = new Scanner(System.in);
+		String cad = "LIFE IS NOT A PROBLEM TO BE SOLVED";
+		int n = in.nextInt();
+		for (int i = 0; i < n; i++) {
+			System.out.print(cad.charAt(i));
+		}
+		System.out.println();
+	}
+
+}
